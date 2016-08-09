a176817dc98fad3a8583e40ca1fcbe5070cc7eea
ahannke <ahannke@gmail.com>
2016-08-09 10:46:44 +0200

    secondSpringProject created

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,29 @@
+package hello;
+
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.*;
+/**
+ * Created by ahann on 09/08/2016.
+ */
+
+@Configuration
+@ComponentScan
+public class Application {
+
+    @Bean
+    MessageService mockMessageService() {
+        return new MessageService() {
+            public String getMessage() {
+                return "Hello World!";
+            }
+        };
+    }
+
+    public static void main(String[] args) {
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext(Application.class);
+        MessagePrinter printer = context.getBean(MessagePrinter.class);
+        printer.printMessage();
+    }
+}
---
+++ src/main/java/hello/MessageService.java
@@ -0,0 +1,8 @@
+package hello;
+
+/**
+ * Created by ahann on 09/08/2016.
+ */
+public interface MessageService {
+    String getMessage();
+}
